a6eb143d0063f92bde3640074922699830a6b64d
Alvin Piter <alpiter10@gmail.com>
2020-09-11 21:19:31 +0800

    Script v3

--- script.sh
+++ script.sh
@@ -1,24 +1,24 @@
 # Check if branch gh-pages exists
 # https://stackoverflow.com/a/28776049
-git rev-parse --verify gh-pages
+git rev-parse --verify gh-pages > /dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
-  git branch -D gh-pages
+  git branch -D gh-pages > /dev/null 2>&1
 fi
 
-git checkout --orphan gh-pages
+git checkout --orphan gh-pages > /dev/null 2>&1
 
-echo 'Generating index.html...\n'
-echo 'Hellllooo' > index.html
-echo 'Generating index.html is done!\n'
+printf 'Generating index.html...\n'
+echo 'Script v3' > index.html
+printf 'Generating index.html is done!\n'
 
-echo 'Pushing changes to remote gh-pages branch...\n'
+printf 'Pushing changes to remote gh-pages branch...\n'
 git add . > /dev/null 2>&1
 git commit -m "Update index.html" > /dev/null 2>&1
 git push origin --force gh-pages > /dev/null 2>&1
-echo 'Pushing changes to remote gh-pages branch is done!\n'
+printf 'Pushing changes to remote gh-pages branch is done!\n'
 
-git checkout master
+git checkout master > /dev/null 2>&1
 
-echo 'All done'
+printf 'All done!\n'
