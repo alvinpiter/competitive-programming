ad5052c035d8a8a744d026bc2e672ef65ab46aff
Alvin Piter <alpiter10@gmail.com>
2020-09-11 23:01:26 +0800

    should work

--- script.sh
+++ script.sh
@@ -1,32 +1,28 @@
 set -e
 
-git status
+printf 'Setting git name and email...\n\n'
+git config user.name
+git config user.email
+printf 'Setting git name and email is done!\n\n'
 
-printf 'Setting git name and email...\n'
-git config user.name "Alvin Piter" > /dev/null 2>&1
-git config user.email "alpiter10@gmail.com" > /dev/null 2>&1
-printf 'Setting git name and email is done!\n'
-
-printf 'Preparing gh-pages branch...\n'
+printf 'Preparing gh-pages branch...\n\n'
 # Check if branch gh-pages exists
 # https://stackoverflow.com/a/28776049
-if git rev-parse --verify gh-pages > /dev/null 2>&1; then
+if git rev-parse --verify gh-pages; then
   git branch -D gh-pages
 fi
 
-git checkout --orphan gh-pages > /dev/null 2>&1
-printf 'Preparing gh-pages branch is done!\n'
+git checkout --orphan gh-pages
+printf 'Preparing gh-pages branch is done!\n\n'
 
-printf 'Generating index.html...\n'
+printf 'Generating index.html...\n\n'
 echo 'Script v5' > index.html
-printf 'Generating index.html is done!\n'
+printf 'Generating index.html is done!\n\n'
 
-printf 'Pushing changes to remote gh-pages branch...\n'
-git add . > /dev/null 2>&1
-git commit -m "Update index.html" > /dev/null 2>&1
+printf 'Pushing changes to remote gh-pages branch...\n\n'
+git add .
+git commit -m "Update index.html"
 git push https://${GITHUB_TOKEN}@github.com/alvinpiter/competitive-programming.git --force gh-pages
-printf 'Pushing changes to remote gh-pages branch is done!\n'
-
-git checkout master
+printf 'Pushing changes to remote gh-pages branch is done!\n\n'
 
 printf 'All done!\n'
